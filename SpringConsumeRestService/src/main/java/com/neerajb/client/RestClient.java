c0d3b55300cda73826610f8b6bb20e9e26649959
Neeraj <bedi.neeraj@gmail.com>
2014-01-27 19:46:48 +0000

    RestClient implemented

--- SpringConsumeRestService/src/main/java/com/neerajb/client/RestClient.java
+++ SpringConsumeRestService/src/main/java/com/neerajb/client/RestClient.java
@@ -1,5 +1,45 @@
 package com.neerajb.client;
 
+import java.util.Map;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
 public class RestClient {
+	private String domain;
+	private RestTemplate restTemplate;
+	
+	public String execute(String path, Map<String, String> headers, Map<String, String> queryParams){
+		
+		UriComponentsBuilder uriBuilder = getUriComponentBuilder(path, headers, queryParams);		
+		HttpEntity<String> requestEntity = new HttpEntity<String>("");		
+		HttpEntity<String> response = getRestTemplate().exchange(uriBuilder.build().encode().toUri(), HttpMethod.GET, requestEntity, String.class);
+		
+		return response.getBody().toString();
+	}
+	
+	private UriComponentsBuilder getUriComponentBuilder(String path, Map<String, String> headers, Map<String, String> queryParams){
+		UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromPath(getDomain()+path);		
+		for(String name : queryParams.keySet()){
+			uriBuilder.queryParam(name, queryParams.get(name));
+		}
+		return uriBuilder;
+	}
+	
+	public String getDomain() {
+		return domain;
+	}
+	public void setDomain(String domain) {
+		this.domain = domain;
+	}
+	public RestTemplate getRestTemplate() {
+		return restTemplate;
+	}
+	public void setRestTemplate(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
 	
 }
--- SpringConsumeRestService/src/main/java/com/neerajb/main/MainClass.java
+++ SpringConsumeRestService/src/main/java/com/neerajb/main/MainClass.java
@@ -3,6 +3,7 @@ package com.neerajb.main;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -10,27 +11,27 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import com.neerajb.client.RestClient;
+
 public class MainClass {
 	public static void main(String[] args) {
-		System.out.println("MainClass.main()");
 		
-		RestTemplate restTemplate = new RestTemplate();
-		Map<String, String> params = new HashMap<String, String>();
-		params.put("issn", "1526-0542");
-//		String str = restTemplate.getForObject("https://secure-qa.jbs.elsevierhealth.com/ae/ecomm/getSubscriptionPrices", String.class, params);
-		
-//		UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromPath("https://secure-qa.jbs.elsevierhealth.com/ae/ecomm/getSubscriptionPrices");
-//		uriBuilder.queryParam("issn", "1526-0542");		
-//		System.out.println(uriBuilder.build().encode().toUri()); 
-		
-		UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromPath("https:////secure-qa.jbs.elsevierhealth.com/ae/ecomm/getSubscriptionPrices");
-		uriBuilder.queryParam("issn", "1526-0542");
-		
-		HttpHeaders headers = new HttpHeaders();
-		HttpEntity<String> requestEntity = new HttpEntity<String>("");
-		
-		HttpEntity<String> response = restTemplate.exchange(uriBuilder.build().encode().toUri(), HttpMethod.GET, requestEntity, String.class);
-		System.out.println(response);
+		ClassPathXmlApplicationContext context=null;
+		try{
+			Map<String, String> queryParams = new HashMap<String, String>();
+			queryParams.put("issn", "1526-0542");
+			
+			context = new ClassPathXmlApplicationContext("spring-config.xml");
+			RestClient restClient = context.getBean("restClient", RestClient.class);
+			String response = restClient.execute("/ae/ecomm/getSubscriptionPrices", null, queryParams);
+			
+			
+			System.out.println(response);
+		}finally{
+			if(context!=null){
+				context.close();
+			}
+		}
 		
 	}
 }
